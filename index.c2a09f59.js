61f2ac3813a2b7937e305dbafdb23100fb363bbc
YuriiPohorilets <102477210+YuriiPohorilets@users.noreply.github.com>
2023-01-06 22:16:07 +0000

    Deploying to gh-pages from @ YuriiPohorilets/starbucks-coffee@9029ae3ade5e8e51d97dbcfb21e0345526b019de 🚀

---
+++ index.c2a09f59.js
@@ -0,0 +1,2 @@
+(()=>{const e=document.querySelector("[js-menu-btn]");e.addEventListener("click",(()=>{const t="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!t),document.body.classList.toggle("menu-is-open")}))})();
+//# sourceMappingURL=index.c2a09f59.js.map
