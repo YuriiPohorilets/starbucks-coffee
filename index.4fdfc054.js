cbd31dcd4b407bf2620b5487dd26795a85f5a453
YuriiPohorilets <102477210+YuriiPohorilets@users.noreply.github.com>
2023-01-08 22:17:29 +0000

    Deploying to gh-pages from @ YuriiPohorilets/starbucks-coffee@7600d86003bc6a4e141876e29644acda601f21e2 🚀

--- index.4fdfc054.js
+++
@@ -1,2 +0,0 @@
-!function(){var e;(e=document.querySelector("[js-menu-btn]")).addEventListener("click",(function(){var t="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!t),document.body.classList.toggle("menu-is-open")}))}();
-//# sourceMappingURL=index.4fdfc054.js.map
---
+++ index.b6767b61.js
@@ -0,0 +1,2 @@
+!function(){var e;(e=document.querySelector("[data-menu-btn]")).addEventListener("click",(function(){var t="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!t),document.body.classList.toggle("menu-is-open")}))}();
+//# sourceMappingURL=index.b6767b61.js.map
